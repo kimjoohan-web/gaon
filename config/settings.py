e150b7f8807067c7c7c62981b4c8a5f821db40bc
김주한 <joodding@naver.com>
2025-11-17 14:05:14 +0900

    config 로컬 주소 수정

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-ex^^ijm7y(x^9(d3f%9*%v4_$f_ibf2e1t*7df($t)+*@y$n=p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.219.50','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.219.50','127.0.0.1','43.201.147.160']
 
 
 # Application definition
