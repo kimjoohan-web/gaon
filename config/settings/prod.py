de35542dade4e85a8a81e98e09d278606873cb2e
김주한 <joodding@naver.com>
2025-11-26 15:34:39 +0900

    base.py static 경로 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,7 @@
 
 from .base import *
 ALLOWED_HOSTS = ['43.201.147.160']
-STATIC_ROOT = BASE_DIR / 'static/'
+# STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
-# STATICFILES_DIRS = []
+STATICFILES_DIRS = []
