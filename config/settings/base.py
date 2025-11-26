fe18d0194960c466718614b790a439c4939eb8f9
김주한 <joodding@naver.com>
2025-11-26 16:14:49 +0900

    base.py static 경로 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -52,8 +52,9 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.messages',
+    
     # 'chat',
     # 'rest_framework',
     
