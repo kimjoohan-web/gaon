d43efafe34d6a5cf44860538d7812417c9354596
김주한 <joodding@naver.com>
2025-11-28 11:06:36 +0900

    base.py 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -144,11 +144,13 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     # BASE_DIR / "static",  
-    os.path.join('/static/'),
+    #os.path.join('/static/'),
+    os.path.join(BASE_DIR, '../static'),
+    './static/',
 ]
 
 # STATIC_ROOT = "static"
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
