c9a6dd53cfa0e5985b4881ee92a5f1a0074853b3
김주한 <joodding@naver.com>
2025-11-28 14:10:09 +0900

    base.py 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-ex^^ijm7y(x^9(d3f%9*%v4_$f_ibf2e1t*7df($t)+*@y$n=p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['43.201.147.160']
 
