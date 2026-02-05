17bca1b39466d657a51b427f1ad8eaa9f9acac6b
김주한 <joodding@naver.com>
2026-02-05 14:53:44 +0900

    redis 서버 전송

--- config/settings/base.py
+++ config/settings/base.py
@@ -174,7 +174,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.environ.get('REDIS_HOST', '127.0.0.1'), 6379)],
+            "hosts": [(os.environ.get('43.201.107.113', '127.0.0.1'), 6379)],
         },
     },
 }
